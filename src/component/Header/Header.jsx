ef13bfa1a454498b8b7161d7ea6a27a26902aee7
unknown <vafaee.mh@gmail.com>
2023-09-09 10:17:06 +0330

    start header app

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerSearch">
+        <div className="headerSearchItem"></div>
+        <div className="headerSearchItem"></div>
+        <div className="headerSearchItem"></div>
+        <div className="headerSearchItem"></div>
+      </div>
+    </div>
+  );
+};
